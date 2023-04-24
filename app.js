6d5184431c485bd2880b5abfd2144c98de07b2c0
Konrad <fprognoz@gmail.com>
2023-04-24 19:32:01 +0300

    refactor(extended-2.1): rename classes according to BEM

--- app.js
+++ app.js
@@ -1,16 +1,7 @@
-//Document is the DOM can be accessed in the console with document.window.
-// Tree is from the top, html, body, p etc.
-
-//Problem: User interaction does not provide the correct results.
-//Solution: Add interactivity so the user can manage daily tasks.
-//Break things down into smaller steps and take each step at a time.
-
-// Event handling, user interaction is what starts the code execution.
-
-var taskInput = document.getElementById("new-task"); //Add a new task.
-var addButton = document.getElementsByTagName("button")[0]; //first button
-var incompleteTaskHolder = document.getElementById("incompleteTasks"); //ul of #incompleteTasks
-var completedTasksHolder = document.getElementById("completed-tasks"); //completed-tasks
+const taskInput = document.getElementById("new-task");
+const addButton = document.querySelector(".button_add");
+const incompleteTaskHolder = document.querySelector(".todo__list");
+const completedTasksHolder = document.querySelector(".completed__list");
 
 //New task list item
 var createNewTaskElement = function (taskString) {
